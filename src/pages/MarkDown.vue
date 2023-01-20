69e2148a53e79bc064b2a55156621951d3d0f94c
Samuel Wabwana <samuel.wabwana@ducommerce.cd>
2023-01-20 14:15:02 +0100

    Debut de l'exercice markdown

---
+++ src/pages/MarkDown.vue
@@ -0,0 +1,17 @@
+<template>
+    <h2>Hello mark down app</h2>
+  <div class="container flex full-height">
+    <textarea name="" id="#in"></textarea>
+    <textarea name="" id="#out"></textarea>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory} from 'vue-router';
 import HomePage from './pages/HomePage.vue';
 import ScrollApp from './pages/ScrollApp.vue';
+import MarkDown from './pages/MarkDown.vue';
 
 const routes = [
     { path: '/', component: HomePage},
-    { path: '/scroll-app', component: ScrollApp}
+    { path: '/scroll-app', component: ScrollApp},
+    { path: '/mark-down', component: MarkDown}
 ];
 const router = createRouter({
     routes,
